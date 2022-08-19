2e1c7c538be7431899666fa695c5872af1916505
TCdotmake <tommyjchen@hotmail.com>
2022-08-18 22:15:21 -0400

    implemented game()

--- index.js
+++ index.js
@@ -9,38 +9,59 @@ function playRound(playerSelection, computerSelection) {
   const winMsg = `You win! ${playerSelection} beats ${computerSelection}!`;
   const loseMsg = `You lost! ${computerSelection} beats ${playerSelection}!`;
   const tieMsg = `You tied! ${playerSelection} vs ${computerSelection}.`;
+  const win = { msg: winMsg, result: 1 };
+  const lose = { msg: loseMsg, result: -1 };
+  const tie = { msg: tieMsg, result: 0 };
   if (player === cpu) {
-    return tieMsg;
+    return tie;
   }
   switch (player) {
     case "ROCK":
       if (cpu === "SCISSORS") {
-        return winMsg;
+        return win;
       }
       break;
     case "PAPER":
       if (cpu === "ROCK") {
-        return winMsg;
+        return win;
       }
       break;
     case "SCISSORS":
       if (cpu === "PAPER") {
-        return winMsg;
+        return win;
       }
       break;
   }
-  return loseMsg;
+  return lose;
 }
 
-console.log(playRound("rock", getComputerChoice()));
-console.log(playRound("rock", getComputerChoice()));
-console.log(playRound("rock", getComputerChoice()));
-console.log(playRound("rock", getComputerChoice()));
-console.log(playRound("paper", getComputerChoice()));
-console.log(playRound("paper", getComputerChoice()));
-console.log(playRound("paper", getComputerChoice()));
-console.log(playRound("paper", getComputerChoice()));
-console.log(playRound("scissors", getComputerChoice()));
-console.log(playRound("scissors", getComputerChoice()));
-console.log(playRound("scissors", getComputerChoice()));
-console.log(playRound("scissors", getComputerChoice()));
+function game() {
+  let player = 0;
+  let cpu = 0;
+  let tie = 0;
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Enter 'Rock', 'Paper', or 'Scissors'...");
+    const round = playRound(playerSelection, getComputerChoice());
+    console.log(round.msg);
+    if (round.result === 0) {
+      tie++;
+    } else if (round.result > 0) {
+      player++;
+    } else {
+      cpu++;
+    }
+  }
+  console.log("Results: ");
+  console.log("player", player);
+  console.log("cpu", cpu);
+  console.log("tie", tie);
+  if (player > cpu) {
+    console.log("You won!");
+  } else if (cpu > player) {
+    console.log("You lose!");
+  } else {
+    console.log("Tie!!");
+  }
+}
+
+game();
