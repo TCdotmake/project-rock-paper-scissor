89049a1daa471405b75e653025db17d356ba76a6
TCdotmake <tommyjchen@hotmail.com>
2022-08-18 21:23:53 -0400

    implemented getComputerChoice()

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const RPS = ["Rock", "Paper", "Scissors"];
+function getComputerChoice() {
+  return RPS[Math.floor(Math.random() * 3)];
+}
+console.log("getComputerChoice()", getComputerChoice());
+console.log("getComputerChoice()", getComputerChoice());
+console.log("getComputerChoice()", getComputerChoice());
+console.log("getComputerChoice()", getComputerChoice());
